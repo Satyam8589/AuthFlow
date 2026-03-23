9a4ae8e314098b7edfcf79fcca97e3e0fd30a9fd
Satyam8589 <satyamkumarsingh9898@gmail.com>
2026-03-23 23:59:44 +0530

    Initial commit: React + Vite + Firebase Auth setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+        <h1>AuthFlow</h1>
+        <p>Login</p>
+      </section>
+    </>
+  )
+}
+
+export default App
